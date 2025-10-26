3612465feeceb195332a3fc7fcbaec6d588005ba
Luis Chinea Rangel <luischinearangel@gmail.com>
2025-10-26 13:13:04 +0000

    Formulario añadido

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { OnInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MaterialFileInputModule } from 'ngx-custom-material-file-input';
+
+const dependencias = [
+    RouterOutlet,
+    MatCardModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MaterialFileInputModule
+]
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [dependencias],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
+
 export class AppComponent implements OnInit {
     title = 'Sistemas de recomendación | Modelos basados en contenido';
 
